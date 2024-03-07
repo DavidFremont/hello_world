48ed61f025902a80668ddbfb7c1fbb0496193c92
DavidFremont <87355037+DavidFremont@users.noreply.github.com>
2024-03-07 22:41:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bạn, Cô, Mẹ,....';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng ngày 8/3';
+const titleCard = 'Tặng bạn thân, người tôi iu thưn';
+const contentCard = 'Chúc những bạn nữ xem được có một cuộc dời thật hạnh phúc, tri thức, xinh đẹp nhó ';
+
+const giftImage = "8thang3.jpg";
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
